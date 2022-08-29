c9ba285ef2dfa6a6f3e727ef9195ea93af6ce0ef
rpineda <>
2022-08-28 21:17:43 -0600

    nodejs/rest-api-003: install dependencies

---
+++ nodejs/rest-api/rest-api-003/_README.sh
@@ -0,0 +1,11 @@
+# rest-api-003 https://www.youtube.com/watch?v=1o9YOHeKhNQ
+
+npm init -y
+npm i express dotenv module-alias mongoose compression cors morgan helmet
+npm i -D typescript tsc-watch eslint prettier eslint-config-prettier eslint-plugin-prettier
+npm i -D @typescript-eslint/parser @typescript-eslint/eslint-plugin @types/node @types/cors \
+@types/express @types/compression @types/morgan
+
+
+# FILES
+to src/ app.ts
